62b7a0c2ecb0abcc995f131b92b5644b61e2e861
ChetanNair <93496359+ChetanNair@users.noreply.github.com>
2024-02-17 03:16:15 -0800

    feat: implement basic app nav structure

--- homemade/App.js
+++ homemade/App.js
@@ -1,23 +1,70 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-export default function App() {
+// Import Screens
+import { LoginScreen } from "./screens/LoginScreen";
+import { SignupScreen } from "./screens/SignupScreen";
+import { HomeScreen } from "./screens/HomeScreen";
+import { DetailScreen } from "./screens/DetailScreen";
+import { ConfirmationScreen } from "./screens/ConfirmationScreen";
+import { MapScreen } from "./screens/MapScreen";
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+// Detail Stack for navigating from Home to Detail and Confirmation without tabs
+function DetailStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="HomeDetail" component={DetailScreen} />
+      <Stack.Screen name="Confirmation" component={ConfirmationScreen} />
+    </Stack.Navigator>
+  );
+}
+
+// Tabs for the main app
+function MainAppTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="HomeTab" component={HomeScreen} />
+      <Tab.Screen name="MapScreen" component={MapScreen} />
+    </Tab.Navigator>
+  );
+}
+
+// Main navigation setup
+function App() {
+  const isSignedIn = true;
   return (
     <SafeAreaProvider>
-      <View style={styles.container}>
-        <Text></Text>
-        <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          {isSignedIn ? (
+            <>
+              <Stack.Screen name="Login" component={LoginScreen} />
+              <Stack.Screen name="Signup" component={SignupScreen} />
+            </>
+          ) : (
+            <>
+              <Stack.Screen
+                name="MainApp"
+                component={MainAppTabs}
+                options={{ headerShown: false }}
+              />
+
+              <Stack.Screen
+                name="DetailStack"
+                component={DetailStack}
+                options={{ headerShown: false, presentation: "modal" }}
+              />
+            </>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
